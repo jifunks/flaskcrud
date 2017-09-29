3f4033194a84ea05e448418bd580c3cb44b44b2c
Jake Funke <jifunks@gmail.com>
2017-09-28 17:20:26 -0700

    Add testing

--- flaskcrud/setup.py
+++ flaskcrud/setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 
 setup(
-        name='flaskcrud',
-        packages=['flaskcrud'],
-        include_package_data=True,
-        install_requires=[
-            'flask',
-        ],
+    name='flaskcrud',
+    packages=['flaskcrud'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+    setup_requires=[
+        'pytest_runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
 )
